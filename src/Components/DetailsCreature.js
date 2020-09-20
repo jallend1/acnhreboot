c605c2f0f6caafaa9f7b162938fc6149e3eb8286
jallend1 <jallend1@gmail.com>
2020-09-20 10:08:06 -0700

    Further expands details page of each individual creature

--- src/Components/DetailsCreature.js
+++ src/Components/DetailsCreature.js
@@ -1,11 +1,25 @@
 import React, { useContext } from 'react';
 import { properCase } from '../utils';
+import Availability from './Creature/Availability';
+import Details from './Creature/Details';
+import ItemContext from '../contexts/ItemContext';
 
 const DetailsCreature = ({ creature }) => {
+  const allItems = useContext(ItemContext);
+  const determineCreatureType = () => {
+    const name = creature.name['name-USen'];
+    allItems.find((item) => item.name['name-USen'] === name);
+  };
   console.log(creature);
   return (
     <div>
       <h5>{properCase(creature.name['name-USen'])}</h5>
+      <div>
+        <Details item={creature} />
+        <table>
+          <Availability item={creature} />
+        </table>
+      </div>
     </div>
   );
 };
