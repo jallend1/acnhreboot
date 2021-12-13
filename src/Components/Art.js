a8827f9796a968340b0aac9231c3f386d4b88293
jallend1 <jallend1@gmail.com>
2021-12-12 23:54:37 -0800

    Updates tasklist comments

--- src/Components/Art.js
+++ src/Components/Art.js
@@ -3,6 +3,8 @@ import { ItemContext } from "../contexts/ItemContext";
 import DetailsArt from "./DetailsArt";
 import Filter from "./Filter";
 
+// TODO: One single component for listing the items, unique components for detail view?
+
 class Art extends React.Component {
   static contextType = ItemContext;
 
